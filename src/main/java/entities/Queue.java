75f2abece97d7b233cdda77793ec1aabe6735fc7
Adam Staňo <adamstano5@gmail.com>
2025-04-21 12:35:01 +0200

    refreshed

--- src/main/java/entities/Queue.java
+++ src/main/java/entities/Queue.java
@@ -5,11 +5,21 @@ import Statistics.TimeWeightedStatistic;
 import java.util.LinkedList;
 
 public class Queue {
-    private LinkedList<Furniture> queue;
-    private TimeWeightedStatistic queueLength;
+    private final LinkedList<Furniture> queue;
+    private final TimeWeightedStatistic queueLength;
 
 
     public Queue(TimeWeightedStatistic queueLength) {
+        this.queueLength = queueLength;
+        this.queue = new LinkedList<>();
+    }
 
+
+    public LinkedList<Furniture> getQueue() {
+        return queue;
+    }
+
+    public TimeWeightedStatistic getQueueLength() {
+        return queueLength;
     }
 }
